e5076677e1f51f7c641ba794ff76c0a7201d512e
FatumaA <67555014+FatumaA@users.noreply.github.com>
2025-06-18 18:44:23 +0300

    refactor: adjust supabase ssr implementation to the later versions

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -25,3 +25,39 @@ export function createClient({
 		}
 	);
 }
+
+// the above wont work with the latest supabase ssr and astro vertsions.
+// you need to use the following code instead:
+// import { createServerClient, parseCookieHeader } from "@supabase/ssr";
+// import type { AstroCookies } from "astro";
+
+// export function createBEClient({
+// 	request,
+// 	cookies,
+// }: {
+// 	request: Request;
+// 	cookies: AstroCookies;
+// }) {
+// 	const cookieHeader = request.headers.get("Cookie") || "";
+
+// 	return createServerClient(
+// 		import.meta.env.SUPABASE_URL,
+// 		import.meta.env.SUPABASE_ANON_KEY,
+// 		{
+// 			cookies: {
+// 				getAll() {
+// 					const cookies = parseCookieHeader(cookieHeader);
+// 					return cookies.map(({ name, value }) => ({
+// 						name,
+// 						value: value ?? "",
+// 					}));
+// 				},
+// 				setAll(cookiesToSet) {
+// 					cookiesToSet.forEach(({ name, value, options }) =>
+// 						cookies.set(name, value, options)
+// 					);
+// 				},
+// 			},
+// 		}
+// 	);
+// }
